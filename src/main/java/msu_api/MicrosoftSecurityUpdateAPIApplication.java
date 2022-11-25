d2a44d4847dc99467e34ff9d160ba72624e57b25
jeffersontdm <jeffersontavares1611@gmail.com>
2022-11-25 12:46:22 -0300

    added cors config

--- src/main/java/msu_api/MicrosoftSecurityUpdateAPIApplication.java
+++ src/main/java/msu_api/MicrosoftSecurityUpdateAPIApplication.java
@@ -1,8 +1,12 @@
 package msu_api;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
